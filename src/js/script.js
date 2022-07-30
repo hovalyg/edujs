698f8e0bb109f971e801b379a213d36895725065
Aldyn Hovalyg <aldyn.hovalyg@gmail.com>
2022-07-31 00:40:36 +0700

    task#3

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,45 @@
-let money = prompt("Ваш бюджет на месяц?", 60);
-let time = prompt("Введите дату в формате YYYY-MM-DD", "2020 - 10 - 10");
-let costName1 = prompt(
-  "Введите обязательную статью расходов в этом месяце #1",
-  "milk"
-);
-let cost1 = prompt("Во сколько обойдется #1?", 10);
-let costName2 = prompt(
-  "Введите обязательную статью расходов в этом месяце #2",
-  "bread"
-);
-let cost2 = prompt("Во сколько обойдется #2?", 15);
+let money = +prompt("Ваш бюджет на месяц?", 50000),
+  time = prompt("Введите дату в формате YYYY-MM-DD", "2020 - 10 - 10");
 
 let appData = {
   budjet: money,
   timeData: time,
-  expenses: {
-    [costName1]: cost1,
-    [costName2]: cost2,
-  },
-  optionalExpenses: "",
-  income: "",
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
   savings: false,
 };
 
-alert(`Ваш бюджет на 1 день составляет ${money / 30}`);
-console.log(appData.expenses);
+for (let i = 0; i < 2; i++) {
+  let costName = prompt(
+      `Введите обязательную статью расходов в этом месяце #${i + 1}`,
+      "milk"
+    ),
+    cost = prompt(`Во сколько обойдется #${i + 1}`, 10);
+
+  if (
+    typeof costName === "string" &&
+    typeof costName != null &&
+    typeof cost != null &&
+    costName != "" &&
+    cost != "" &&
+    costName.length < 50
+  ) {
+    console.log("done");
+    appData.expenses[costName] = cost;
+  }
+}
+
+appData.moneyPerDay = appData.budjet / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Ошибка");
+}
